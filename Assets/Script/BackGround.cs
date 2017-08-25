02d4c929aea00926238709d8a48e91ea8db7f30f
pbasson <preet.basson@gmail.com>
2017-08-25 11:20:33 +0100

    Background start screen

--- Assets/Script/BackGround.cs
+++ Assets/Script/BackGround.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BackGround : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
 	}
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+  void Update()
+  {
+		float sinT = Mathf.Sin(Time.time );
+		transform.position = new Vector2(0, sinT*10f);
 
-
-    }
+  }
 
 
 }
