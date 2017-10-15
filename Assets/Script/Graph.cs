602e3c8ca160a10ed9f4a7dc2099b23623163bfd
Preet <preet.basson@gmail.com>
2017-10-15 17:10:09 +0100

    Cleaned code

--- Assets/Script/Graph.cs
+++ Assets/Script/Graph.cs
@@ -13,88 +13,39 @@ public class Graph : MonoBehaviour
     public GameObject EndPrefab;
     public GameObject collectable;
     public GameObject EnemyPrefab;
+    public static IntVec2 size;
     public int x, y;
-    public GameObject[] collectT = new GameObject[10];
 
     private List<GameObject> collectables = new List<GameObject>();
     private List<GameObject> enemyList = new List<GameObject>();
-    private GameObject[] enemyArray = new GameObject[10];
-    private int enemyAmount=0;
     private GameObject[,] cell;
     private bool[,] isSolid ;
-    public static IntVec2 size;
-    public static int sizeTo=1;
-
 
     public void Generate()
     {
         size = new IntVec2(x,y);
         cell = new GameObject[size.x, size.y];
         Step(size);
-        SpawnStep(size);
-        CollectStep(size);
-        EnemySet(size);
-        //BackgroundStep(size);
+        SpawnCol(size, EnemyPrefab, enemyList);
+        SpawnCol(size, collectable, collectables);
     }
 
-    private void EnemySet(IntVec2 cord)
+    private void SpawnCol(IntVec2 cord, GameObject gamePrefab, List<GameObject> gameList)
     {
-      if( enemyList.Count< size.x)
-      {
-        for (int i = 0; i < size.x/2; i++)
-        {
-          for (int j = 0; j < size.y; j++)
-           {
-            int R1 = Random.Range(0, size.x);
-            int R2 = Random.Range(0, size.y);
-            cord = new IntVec2(R1, R2);
-            if ( isSolid[R1,R2] )
-            {
-                        //CreateEnemy(cord);
-                        CreateDuo(cord, EnemyPrefab, enemyList);
-            }
-          }
-        }
-      }
-    }
-
-
-    private void CollectStep(IntVec2 cord)
-    {
-      //if (collectables.Count < 10)
-    //  {
         for (int i = 0; i < size.x /2; i++)
         {
-          for (int j = 0; j < size.y; j++)
-          {
-            int R1 = Random.Range(0,size.x);
-            int R2 = Random.Range(0, size.y);
-            cord = new IntVec2(R1, R2);
-            if (isSolid[R1,R2])
+            for (int j = 0; j < size.y; j++)
             {
-                    //CreateCollect(cord);
-                    CreateDuo(cord,collectable,collectables);
+                int R1 = Random.Range(0,size.x);
+                int R2 = Random.Range(0, size.y);
+                cord = new IntVec2(R1, R2);
+                if (isSolid[R1,R2])
+                {
+                        CreateDuo(cord,gamePrefab, gameList);
+                }
             }
-          }
         }
-      //}
-    }
-
-
-    private void BackgroundStep(IntVec2 cord)
-    {
-      int sizeH = size.y / 2;
-      for (int i = 0; i < size.x; i++)
-      {
-        for (int j = 0; j < size.y; j++)
-        {
-          if (j % 13 ==0)
-          {
-            cord = new IntVec2(i, j);
-           // CreateBackground(cord);
-          }
-        }
-      }
+      
     }
 
     private void Step(IntVec2 cord)
@@ -105,111 +56,84 @@ public class Graph : MonoBehaviour
           for (int j = 0; j < size.y; j++)
           {
               cord = new IntVec2(i, j);
-
-                if (j == 0)
-                {
-                    CreateLevel(cord, GroundPrefab, av.Names.ground);
-                }
-                   // CreateFloor(cord);}
-
-              if (j == size.y - 1)
-              {
-                    CreateLevel(cord, CeilingPrefab, av.Names.ceiling);
-                   // CreateCeiling(cord);
-                }
-
-              if( j <= size.y && i == 0 )
-              {
-                    CreateLevel(cord, StartPrefab, av.Names.start, av.Names.startVal);
-                    //CreateStart(cord);
-                }
-
-              if( i == size.x - 1 )
-              {
-                    CreateLevel(cord, EndPrefab, av.Names.end, av.Names.endVal );
-                 //   CreateEnd(cord);
-                }
-                if (j % 13 == 0)
-                {
-                    CreateLevel(cord, BackgroundPrefab, av.Names.backGround, 0, av.Names.bgVal);
-                }
-           }
+              SpawnLevel(cord, i, j);
+              SpawnPlatform(cord, i, j);
+          }
         }
     }
 
-
-
-    private void SpawnStep(IntVec2 cord)
+    private void SpawnLevel(IntVec2 cord, int i, int j)
     {
-        for (int i = 0; i < size.x; i++)
+        if (j == 0)
+        { CreateLevel(cord, GroundPrefab, av.Names.ground); }
+
+        if (j == size.y - 1)
+        { CreateLevel(cord, CeilingPrefab, av.Names.ceiling); }
+
+        if (j <= size.y && i == 0)
+        { CreateLevel(cord, StartPrefab, av.Names.start, av.Names.startVal); }
+
+        if (i == size.x - 1)
+        { CreateLevel(cord, EndPrefab, av.Names.end, av.Names.endVal); }
+
+        if (j % 13 == 0)
+        { CreateLevel(cord, BackgroundPrefab, av.Names.backGround, 0, av.Names.bgVal); }
+    }
+
+    private void SpawnPlatform(IntVec2 cord, int i, int j)
+    {
+        if (i >= 2 && j >= 1 )
         {
-            for (int j = 0; j < size.y-4; j++)
+
+            if (i % 2 == 0 && j == 4)
             {
-                cord = new IntVec2(i,j);
-                if (i >= 2 && j >= 1)
-                {
+                CreateSpawn(cord);
+            }
+            else if (j == 8 && i % 2 != 0)
+            {
+                CreateSpawn(cord);
+            }
+            else if (i > 2 && i % 2 == 0 && j == 12 && i % 4 == 0)
+            {
+                CreateSpawn(cord);
+            }
+            else if (j == 16 && i % 2 != 0 && i > 4)
+            {
+                CreateSpawn(cord);
+            }
+            else if (j == 20 && i % 2 == 0 && i > 6)
+            {
+                CreateSpawn(cord);
+            }
+            else if (j == 24 && i % 2 != 0 && i % 4 != 0 && i > 8)
+            {
+                CreateSpawn(cord);
+            }
+            else if (j == 28 && i % 2 == 0 && i % 4 == 0 && i > 10)
+            {
+                CreateSpawn(cord);
+            }
+            else if (j == 32 && i % 2 != 0 && i > 12)
+            {
+                CreateSpawn(cord);
+            }
+            else if (j == 36 && i % 2 == 0 && i > 14)
+            {
+                CreateSpawn(cord);
+            }
+            else if (j == 40 && i % 2 != 0 && i > 16)
+            {
+                CreateSpawn(cord);
+            }
+            else if (j == 44 && i % 2 == 0 && i > 18)
+            {
+                CreateSpawn(cord);
+            }
+            else if (j == size.y - 1 && i == size.x - 1)
+            {
 
-                    if ( i % 2==0 && j == 4 )
-                    {
-                        CreateSpawn(cord);
-                    }
-                    else if (j == 8 && i % 2 != 0 ) {
-                        CreateSpawn(cord);
-                    }
-                    else if( i > 2 && i % 2 == 0 && j == 12 && i % 4 ==0 ){
-                        CreateSpawn(cord);
-                    }
-                    else if ( j == 16 && i % 2 != 0  && i > 4 ) {
-                        CreateSpawn(cord);
-                    }
-                    else if ( j == 20 && i % 2 == 0 && i> 6 ) {
-                      CreateSpawn(cord);
-                    }
-                    else if (j == 24 && i % 2 != 0 && i % 4 !=0  && i > 8 ) {
-                      CreateSpawn(cord);
-                    }
-                    else if (j == 28 && i % 2 == 0 && i % 4 == 0 && i > 10 ) {
-                      CreateSpawn(cord);
-                    }
-                    else if (j == 32 && i % 2 != 0  && i > 12 ) {
-                      CreateSpawn(cord);
-                    }
-                    else if (j == 36 && i % 2 == 0  && i > 14 ) {
-                      CreateSpawn(cord);
-                    }
-                    else if (j == 40 && i % 2 != 0  && i > 16 ) {
-                      CreateSpawn(cord);
-                    }
-                    else if (j == 44 && i % 2 == 0  && i > 18 ) {
-                      CreateSpawn(cord);
-                    }
-                    else if (j == size.y -1 && i == size.x-1 ) {
-
-                    }
-                  }
             }
         }
-    }
-
-    private void CreateEnemy(IntVec2 cord)
-    {
-        GameObject enemy = Instantiate(EnemyPrefab) as GameObject;
-        cell[cord.x, cord.y] = enemy;
-        enemy.name = "Enemy: " + cord.x + ", " + cord.y;
-        enemy.transform.localPosition = new Vector2(cord.x * 11.5f, cord.y + 2f);
-        enemy.transform.parent = transform;
-        isSolid[cord.x, cord.y] = false;
-        enemyList.Add(EnemyPrefab);
-    }
-
-    private void CreateCollect(IntVec2 cord)
-    {
-        GameObject collect = Instantiate(collectable) as GameObject;
-        cell[cord.x, cord.y] = collect;
-        collect.transform.localPosition = new Vector2(cord.x * 11.5f, cord.y+2f);
-        collect.transform.parent = transform;
-        isSolid[cord.x, cord.y] = false;
-        collectables.Add(collectable);
     }
 
     private void CreateDuo(IntVec2 cord, GameObject duoPrefab, List<GameObject> gameList)
@@ -222,43 +146,6 @@ public class Graph : MonoBehaviour
         gameList.Add(duoPrefab);
     }
 
-    private void CreateFloor(IntVec2 cord)
-    {
-        GameObject floor = Instantiate(GroundPrefab) as GameObject; //Create Instance of the Floor
-        cell[cord.x, cord.y] = floor;
-        floor.name = "Floor: " + cord.x +", "+ cord.y;     //Label for the Scene Manager
-        floor.transform.localPosition = new Vector2(cord.x*11.5f,cord.y); //Use of Cord.x * value (Scaler) made spacing
-        floor.transform.parent = transform; //Set to all to tree under the Graph
-
-    }
-
-    private void CreateStart(IntVec2 cord)
-    {
-        GameObject start = Instantiate(StartPrefab) as GameObject;
-        cell[cord.x, cord.y] = start;
-        start.name = "Start: " + cord.x + ", "+ cord.y;
-        start.transform.localPosition = new Vector2(cord.x*11.5f-5f, cord.y);
-        start.transform.parent = transform;
-    }
-
-    private void CreateEnd(IntVec2 cord)
-    {
-        GameObject end = Instantiate(StartPrefab) as GameObject;
-        cell[cord.x, cord.y] = end;
-        end.name = "end: " + cord.x + ", "+ cord.y;
-        end.transform.localPosition = new Vector2(cord.x*11.5f+5f, cord.y);
-        end.transform.parent = transform;
-    }
-
-    private void CreateCeiling(IntVec2 cord)
-    {
-        GameObject ceiling = Instantiate(CeilingPrefab) as GameObject;
-        cell[cord.x, cord.y] = ceiling;
-        ceiling.name = "Ceiling: " + cord.x + ", "+ cord.y;
-        ceiling.transform.localPosition = new Vector2(cord.x * 11.5f, cord.y);
-        ceiling.transform.parent = transform;
-    }
-
     private void CreateSpawn(IntVec2 cord)
     {
         GameObject spawn = Instantiate(SpawnPrefab) as GameObject;
@@ -269,16 +156,7 @@ public class Graph : MonoBehaviour
         isSolid[cord.x,cord.y]= true;
     }
 
-    private void CreateBackground(IntVec2 cord)
-    {
-        GameObject background = Instantiate(BackgroundPrefab) as GameObject;
-        cell[cord.x, cord.y] = background;
-        background.name = "Background: " + cord.x + ", " + cord.y;
-        background.transform.localPosition = new Vector2(cord.x* 11.5f, cord.y+6f);
-        background.transform.parent = transform;
-    }
-
-    private void CreateLevel(IntVec2 cord, GameObject gamePrefab, string name, float space = 0, float back= 0 )
+    private void CreateLevel(IntVec2 cord, GameObject gamePrefab, string name, float space = 0, float back= 0)
     {
         GameObject gameInst = Instantiate(gamePrefab) as GameObject;
         cell[cord.x, cord.y] = gameInst;
